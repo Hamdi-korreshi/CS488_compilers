19a555b949eaf8834baa3f2a2e20fa67ad5e40d1
Hamdi-korreshi <hamdikorreshi@gmail.com>
2024-10-15 21:24:47 -0400

    fixed dat cycle

--- PA2/tom_idea.ml
+++
@@ -1,8 +0,0 @@
-and cool_class = id * (id option) * feature list
-
-get attrs in parent class (feature list) then just prepend to the list of class child
-then run redef on all attrs?
-
-get methods in parent class (method list) then just prepend to the list of class child
-then run redef on all methods? (check if formals are the same then return type is the same)
-
