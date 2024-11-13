e213faf4c536a091a93aa0d64a8d525cbed4b4c7
tomaszbrauntsch <tomaszbrauntsch@gmail.com>
2024-11-12 23:43:17 -0500

    added-intial-tables-reg-stuff

--- PA3/PA3c2/main.ml
+++ PA3/PA3c2/main.ml
@@ -113,21 +113,6 @@ let debug tac =
         printf "return %s\n" var
     | _ -> ()
 
-  (* TODO
-    Branches, Labels, and Control Flow:
-    jmp label - this happens right after the conditional body finishes
-    label label - this is the "function Label" - after the conditional is done then write this Label
-    return x - only occurs once per method, somewhat handled by current program
-    comment ...text until end of line... - some arbrituary program that gives context
-    bt x label - if a conditional appears then there is a contradiction variable (that acts as the false) and then the bt's are listed right after that
-
-    Implement way for methods
-    Handle not and negate
-    Handle new and default
-    Research new type and default type
-    Stack the methods into one program
-  *)
-
 let fresh_variable =
   let counter = ref 0 in
   fun () ->
@@ -221,6 +206,17 @@ type attr_table = (string, string) Hashtbl.t
 (* let is_digit num = 
   let jack = Str.string_match (Str.regexp "123456789") num 0 in
   jack  *)
+
+type var_to_rsp = (string, string) Hashtbl.t (* variable name, rsp w/ offset*)
+type var_to_reg = (string, string) Hashtbl.t (* variable name, register name *)
+let curr_rsp_max_offset = ref 0 (* The current method's stack's allocation for vars *)
+(*  Note
+Use stack for local vars
+any arguments for a function will be the according regs (rdi, rsi, etc. until six then think from there)
+Assign the last / method in the method to %rax then pop off the stack to release the stack allocation
+Refer to the takeout.s for the correct placement of .globl Main.main / Main.main.end and Main.main in accordance to asm
+We might be able to change the positioning of these tables and still be fine
+*)
 
 let print_cool_type (loc, tname) =
   Printf.printf "Cool_Type (location: %s, type: %s)\n" loc tname
@@ -1148,3 +1144,474 @@ let main () = begin
         ) ast;
         end ;;
 main();;
+
+let initial_vtable_before_main = "                        ## ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.globl Bool..vtable
+Bool..vtable:           ## virtual function table for Bool
+                        .quad string2
+                        .quad Bool..new
+                        ## ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.globl IO..vtable
+IO..vtable:             ## virtual function table for IO
+                        .quad string3
+                        .quad IO..new
+                        .quad IO.in_int
+                        .quad IO.out_int
+                        .quad IO.out_string
+                        ## ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.globl Int..vtable
+Int..vtable:            ## virtual function table for Int
+                        .quad string4
+                        .quad Int..new
+                        ## ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.globl Main..vtable
+Main..vtable:           ## virtual function table for Main
+                        .quad string5
+                        .quad Main..new
+                        .quad IO.in_int
+                        .quad IO.out_int
+                        .quad Main.main
+                        ## ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.globl Bool..new
+Bool..new:              ## constructor for Bool
+                        pushq %rbp
+                        movq %rsp, %rbp
+                        ## stack room for temporaries: 1
+                        movq $8, %r14
+                        subq %r14, %rsp
+                        ## return address handling
+                        movq $4, %r12
+                        movq $8, %rsi
+			movq %r12, %rdi
+			call calloc
+			movq %rax, %r12
+                        ## store class tag, object size and vtable pointer
+                        movq $0, %r14
+                        movq %r14, 0(%r12)
+                        movq $4, %r14
+                        movq %r14, 8(%r12)
+                        movq $Bool..vtable, %r14
+                        movq %r14, 16(%r12)
+                        ## initialize attributes
+                        ## self[3] holds field (raw content) (Int)
+                        movq $0, %r13
+                        movq %r13, 24(%r12)
+                        ## self[3] (raw content) initializer -- none 
+                        movq %r12, %r13
+                        ## return address handling
+                        movq %rbp, %rsp
+                        popq %rbp
+                        ret
+                        ## ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.globl IO..new
+IO..new:                ## constructor for IO
+                        pushq %rbp
+                        movq %rsp, %rbp
+                        ## stack room for temporaries: 1
+                        movq $8, %r14
+                        subq %r14, %rsp
+                        ## return address handling
+                        movq $3, %r12
+                        movq $8, %rsi
+			movq %r12, %rdi
+			call calloc
+			movq %rax, %r12
+                        ## store class tag, object size and vtable pointer
+                        movq $11, %r14
+                        movq %r14, 0(%r12)
+                        movq $3, %r14
+                        movq %r14, 8(%r12)
+                        movq $IO..vtable, %r14
+                        movq %r14, 16(%r12)
+                        movq %r12, %r13
+                        ## return address handling
+                        movq %rbp, %rsp
+                        popq %rbp
+                        ret
+                        ## ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.globl Int..new
+Int..new:               ## constructor for Int
+                        pushq %rbp
+                        movq %rsp, %rbp
+                        ## stack room for temporaries: 1
+                        movq $8, %r14
+                        subq %r14, %rsp
+                        ## return address handling
+                        movq $4, %r12
+                        movq $8, %rsi
+			movq %r12, %rdi
+			call calloc
+			movq %rax, %r12
+                        ## store class tag, object size and vtable pointer
+                        movq $1, %r14
+                        movq %r14, 0(%r12)
+                        movq $4, %r14
+                        movq %r14, 8(%r12)
+                        movq $Int..vtable, %r14
+                        movq %r14, 16(%r12)
+                        ## initialize attributes
+                        ## self[3] holds field (raw content) (Int)
+                        movq $0, %r13
+                        movq %r13, 24(%r12)
+                        ## self[3] (raw content) initializer -- none 
+                        movq %r12, %r13
+                        ## return address handling
+                        movq %rbp, %rsp
+                        popq %rbp
+                        ret
+                        ## ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.globl Main..new
+Main..new:              ## constructor for Main
+                        pushq %rbp
+                        movq %rsp, %rbp
+                        ## stack room for temporaries: 1
+                        movq $8, %r14
+                        subq %r14, %rsp
+                        ## return address handling
+                        movq $5, %r12
+                        movq $8, %rsi
+			movq %r12, %rdi
+			call calloc
+			movq %rax, %r12
+                        ## store class tag, object size and vtable pointer
+                        movq $12, %r14
+                        movq %r14, 0(%r12)
+                        movq $5, %r14
+                        movq %r14, 8(%r12)
+                        movq $Main..vtable, %r14
+                        movq %r14, 16(%r12)
+                        ## initialize attributes
+                        ## self[3] holds field y (Int)
+                        ## new Int
+                        pushq %rbp
+                        pushq %r12
+                        movq $Int..new, %r14
+                        call *%r14
+                        popq %r12
+                        popq %rbp
+                        movq %r13, 24(%r12)
+                        ## self[4] holds field x (Int)
+                        ## new Int
+                        pushq %rbp
+                        pushq %r12
+                        movq $Int..new, %r14
+                        call *%r14
+                        popq %r12
+                        popq %rbp
+                        movq %r13, 32(%r12)
+                        ## self[3] y initializer -- none 
+                        ## self[4] x initializer -- none 
+                        movq %r12, %r13
+                        ## return address handling
+                        movq %rbp, %rsp
+                        popq %rbp
+                        ret
+                        ## ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.globl IO.in_int
+IO.in_int:              ## method definition
+                        pushq %rbp
+                        movq %rsp, %rbp
+                        movq 16(%rbp), %r12
+                        ## stack room for temporaries: 1
+                        movq $8, %r14
+                        subq %r14, %rsp
+                        ## return address handling
+                        ## method body begins
+                        ## new Int
+                        pushq %rbp
+                        pushq %r12
+                        movq $Int..new, %r14
+                        call *%r14
+                        popq %r12
+                        popq %rbp
+                        movq %r13, %r14
+                        movl	$1, %esi
+			movl $4096, %edi
+			call calloc
+			pushq %rax
+			movq %rax, %rdi
+			movq $4096, %rsi 
+			movq stdin(%rip), %rdx
+			call fgets 
+			popq %rdi 
+			movl $0, %eax
+			pushq %rax
+			movq %rsp, %rdx
+			movq $percent.ld, %rsi
+			call sscanf
+			popq %rax
+			movq $0, %rsi 
+			cmpq $2147483647, %rax 
+			cmovg %rsi, %rax
+			cmpq $-2147483648, %rax 
+			cmovl %rsi, %rax
+			movq %rax, %r13
+                        movq %r13, 24(%r14)
+                        movq %r14, %r13
+.globl IO.in_int.end
+IO.in_int.end:          ## method body ends
+                        ## return address handling
+                        movq %rbp, %rsp
+                        popq %rbp
+                        ret
+                        ## ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.globl IO.out_int
+IO.out_int:             ## method definition
+                        pushq %rbp
+                        movq %rsp, %rbp
+                        movq 16(%rbp), %r12
+                        ## stack room for temporaries: 1
+                        movq $8, %r14
+                        subq %r14, %rsp
+                        ## return address handling
+                        ## fp[3] holds argument x (Int)
+                        ## method body begins
+                        movq 24(%rbp), %r14
+                        movq 24(%r14), %r13
+                        movq $percent.d, %rdi
+		movl %r13d, %eax
+		cdqe
+		movq %rax, %rsi
+			movl $0, %eax
+			call printf
+                        movq %r12, %r13
+.globl IO.out_int.end
+IO.out_int.end:         ## method body ends
+                        ## return address handling
+                        movq %rbp, %rsp
+                        popq %rbp
+                        ret" in
+let initial_vtable_after_main = "                        ## global string constants
+.globl string2
+string2:                # 'Bool'
+.byte  66 # 'B'
+.byte 111 # 'o'
+.byte 111 # 'o'
+.byte 108 # 'l'
+.byte 0
+
+.globl string3
+string3:                # 'IO'
+.byte  73 # 'I'
+.byte  79 # 'O'
+.byte 0
+
+.globl string4
+string4:                # 'Int'
+.byte  73 # 'I'
+.byte 110 # 'n'
+.byte 116 # 't'
+.byte 0
+
+.globl string5
+string5:                # 'Main'
+.byte  77 # 'M'
+.byte  97 # 'a'
+.byte 105 # 'i'
+.byte 110 # 'n'
+.byte 0
+
+                        ## ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.globl eq_handler
+eq_handler:             ## helper function for =
+                        pushq %rbp
+                        movq %rsp, %rbp
+                        movq 32(%rbp), %r12
+                        ## return address handling
+                        movq 32(%rbp), %r13
+                        movq 24(%rbp), %r14
+                        cmpq %r14, %r13
+			je eq_true
+                        movq $0, %r15
+                        cmpq %r15, %r13
+			je eq_false
+                        cmpq %r15, %r14
+			je eq_false
+                        movq 0(%r13), %r13
+                        movq 0(%r14), %r14
+                        ## place the sum of the type tags in r1
+                        addq %r14, %r13
+                        movq $0, %r14
+                        cmpq %r14, %r13
+			je eq_bool
+                        movq $2, %r14
+                        cmpq %r14, %r13
+			je eq_int
+                        movq $6, %r14
+                        cmpq %r14, %r13
+			je eq_true
+.globl eq_false
+eq_false:               ## not equal
+                        ## new Bool
+                        pushq %rbp
+                        pushq %r12
+                        movq $Bool..new, %r14
+                        call *%r14
+                        popq %r12
+                        popq %rbp
+                        jmp eq_end
+.globl eq_true
+eq_true:                ## equal
+                        ## new Bool
+                        pushq %rbp
+                        pushq %r12
+                        movq $Bool..new, %r14
+                        call *%r14
+                        popq %r12
+                        popq %rbp
+                        movq $1, %r14
+                        movq %r14, 24(%r13)
+                        jmp eq_end
+.globl eq_bool
+eq_bool:                ## two Bools
+.globl eq_int
+eq_int:                 ## two Ints
+                        movq 32(%rbp), %r13
+                        movq 24(%rbp), %r14
+                        movq 24(%r13), %r13
+                        movq 24(%r14), %r14
+                        cmpq %r14, %r13
+			je eq_true
+                        jmp eq_false
+.globl eq_end
+eq_end:                 ## return address handling
+                        movq %rbp, %rsp
+                        popq %rbp
+                        ret
+                        ## ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.globl le_handler
+le_handler:             ## helper function for <=
+                        pushq %rbp
+                        movq %rsp, %rbp
+                        movq 32(%rbp), %r12
+                        ## return address handling
+                        movq 32(%rbp), %r13
+                        movq 24(%rbp), %r14
+                        cmpq %r14, %r13
+			je le_true
+                        movq $0, %r15
+                        cmpq %r15, %r13
+			je le_false
+                        cmpq %r15, %r14
+			je le_false
+                        movq 0(%r13), %r13
+                        movq 0(%r14), %r14
+                        ## place the sum of the type tags in r1
+                        addq %r14, %r13
+                        movq $0, %r14
+                        cmpq %r14, %r13
+			je le_bool
+                        movq $2, %r14
+                        cmpq %r14, %r13
+			je le_int
+                        movq $6, %r14
+                        cmpq %r14, %r13
+			je le_true
+.globl le_false
+le_false:               ## not less-than-or-equal
+                        ## new Bool
+                        pushq %rbp
+                        pushq %r12
+                        movq $Bool..new, %r14
+                        call *%r14
+                        popq %r12
+                        popq %rbp
+                        jmp le_end
+.globl le_true
+le_true:                ## less-than-or-equal
+                        ## new Bool
+                        pushq %rbp
+                        pushq %r12
+                        movq $Bool..new, %r14
+                        call *%r14
+                        popq %r12
+                        popq %rbp
+                        movq $1, %r14
+                        movq %r14, 24(%r13)
+                        jmp le_end
+.globl le_bool
+le_bool:                ## two Bools
+.globl le_int
+le_int:                 ## two Ints
+                        movq 32(%rbp), %r13
+                        movq 24(%rbp), %r14
+                        movq 24(%r13), %r13
+                        movq 24(%r14), %r14
+                        cmpl %r14d, %r13d
+			jle le_true
+                        jmp le_false
+.globl le_end
+le_end:                 ## return address handling
+                        movq %rbp, %rsp
+                        popq %rbp
+                        ret
+                        ## ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.globl lt_handler
+lt_handler:             ## helper function for <
+                        pushq %rbp
+                        movq %rsp, %rbp
+                        movq 32(%rbp), %r12
+                        ## return address handling
+                        movq 32(%rbp), %r13
+                        movq 24(%rbp), %r14
+                        movq $0, %r15
+                        cmpq %r15, %r13
+			je lt_false
+                        cmpq %r15, %r14
+			je lt_false
+                        movq 0(%r13), %r13
+                        movq 0(%r14), %r14
+                        ## place the sum of the type tags in r1
+                        addq %r14, %r13
+                        movq $0, %r14
+                        cmpq %r14, %r13
+			je lt_bool
+                        movq $2, %r14
+                        cmpq %r14, %r13
+			je lt_int
+                        movq $6, %r14
+                        cmpq %r14, %r13
+.globl lt_false
+lt_false:               ## not less than
+                        ## new Bool
+                        pushq %rbp
+                        pushq %r12
+                        movq $Bool..new, %r14
+                        call *%r14
+                        popq %r12
+                        popq %rbp
+                        jmp lt_end
+.globl lt_true
+lt_true:                ## less than
+                        ## new Bool
+                        pushq %rbp
+                        pushq %r12
+                        movq $Bool..new, %r14
+                        call *%r14
+                        popq %r12
+                        popq %rbp
+                        movq $1, %r14
+                        movq %r14, 24(%r13)
+                        jmp lt_end
+.globl lt_bool
+lt_bool:                ## two Bools
+.globl lt_int
+lt_int:                 ## two Ints
+                        movq 32(%rbp), %r13
+                        movq 24(%rbp), %r14
+                        movq 24(%r13), %r13
+                        movq 24(%r14), %r14
+                        cmpl %r14d, %r13d
+			jl lt_true
+                        jmp lt_false
+.globl lt_end
+lt_end:                 ## return address handling
+                        movq %rbp, %rsp
+                        popq %rbp
+                        ret
+                        ## ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.globl start
+start:                  ## program begins here
+                        .globl main
+			.type main, @function" in
+printf "sample text";
