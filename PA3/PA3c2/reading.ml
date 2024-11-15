03f796c1f7ceb6daa8b9ae42e6104d3e18e5502a
Hamdi-korreshi <hamdikorreshi256@gmail.com>
2024-11-15 18:39:34 -0500

    completed the work on reading in class map

--- PA3/PA3c2/reading.ml
+++ PA3/PA3c2/reading.ml
@@ -53,8 +53,10 @@ and exp_kind =
   | Not of exp
   | Internal of cool_type * string * string
 
-  type class_map = (string, (string * string * exp option) list option) Hashtbl.t
-  let main () = begin
+type class_map = (string, (string * string * exp option) list option) Hashtbl.t
+type implementation_map = (string, (string * (string * string) list * string * exp) list) Hashtbl.t
+
+let main () = begin
   let fname = Sys.argv.(1) in
   let fin = open_in fname in
 
@@ -78,138 +80,173 @@ and exp_kind =
     let name = read() in
     (loc, name)
   in
-  let rec read_exp () =
+  let rec read_exp read =
+    (* Read location *)
     let eloc = read () in
-    let ekind = match read () with
-    | "not" ->
-      let e = read_exp () in 
-      Not(e)
-    | "integer" ->
-        let ival = read () in 
-        Integer(ival)
-    | "string" ->
-        let ival = read () in 
-        String(ival)
-    | "true" ->
-        Bool("true")
-    | "false" ->
-        Bool("true")
-    | "negate" ->
-      let ival = read_exp() in
-      Negate(ival)
-    | "lt" ->
-      (* Get the type of the datatype then push into bool_error *)
-      let ival = read_exp() in
-      let xval = read_exp() in
-      LT(ival, xval)
-    | "le" ->
-      (* Get the type of the datatype then push into bool_error *)
-      let ival = read_exp() in
-      let xval = read_exp() in 
-      LE(ival, xval)
-    | "eq" ->
-      (* Get the type of the datatype then push into bool_error *)
-      let ival = read_exp() in
-      let xval = read_exp() in 
-      EQ(ival, xval)
-    | "assign" ->
-      let var = read_id () in 
-      let rhs_exp = read_exp () in 
-      Assign(var,rhs_exp)
-    | "isvoid" -> 
-      let void = read_exp () in 
-      Isvoid(void)
-    | "if" ->
-      let if_exp = read_exp () in 
-      let then_exp = read_exp () in 
-      let else_exp = read_exp () in 
-      If(if_exp,then_exp,else_exp)
-    | "while" ->
-      let loop = read_exp () in 
-      let pool = read_exp () in 
-      While(loop,pool)
-    | "block" ->
-      let amount_to_read = int_of_string(read ()) in
-      let rec read_block n acc =
-        if n <= 0 then List.rev acc
-        else (
-          let expr = read_exp () in 
-          read_block (n-1) (expr :: acc)
-        ) in 
-      let exp_list = read_block amount_to_read [] in
-      Block(exp_list)
-    | "case" ->
-      let test_exp = read_exp () in 
-      let read_case () = 
-        let case_var = read_id () in 
-        let case_type = read_id () in 
-        let case_exp = read_exp () in 
-        (case_var, case_type, case_exp)
-      in 
-      let case_list = read_list read_case in
-      Case(test_exp,case_list)
-    | "dynamic_dispatch" ->
-      let e = read_exp () in
-      let metho = read_id () in 
-      (* print_id metho; *)
-      let args = read_list read_exp in 
-      Dynamic_Dispatch(e,metho,args)
-    | "static_dispatch" ->
-      let e = read_exp () in
-      let t_static = read_id () in
-      let metho = read_id () in 
-      let args = read_list read_exp in 
-      Static_Dispatch(e,t_static,metho,args)
-    | "self_dispatch" ->
-      let metho = read_id () in 
-      let args = read_list read_exp in 
-      Self_Dispatch(metho,args)
-    | "let" ->
-        let num_bindings = int_of_string (read ()) in
-        let rec binding_list n acc =
-          if n <= 0 then List.rev acc
-          else (
-            let lbni = read () in 
-            let let_var = read_id () in 
-            let let_type = read_id () in 
-            let binding = 
-              match lbni with
-              | "let_binding_no_init" -> (let_var, let_type, None)
-              | "let_binding_init" ->
-                let init_exp = read_exp () in 
-                (let_var, let_type, Some init_exp)
-              | _ -> (let_var, let_type, None) (* failwith "binding failed, invalid let" *)
-              in 
-              binding_list (n-1) (binding :: acc)
-          ) in
-        let bindings = binding_list num_bindings [] in 
-        let let_body = read_exp () in
-        Let(bindings, let_body)
-    | "plus" -> (* might have to change all of these*)
-        let ival = read_exp() in
-        let xval = read_exp() in
-        Plus(ival, xval)
-    | "minus" ->
-        let ival = read_exp() in
-        let xval = read_exp() in
-        Minus(ival, xval)
-    | "times" -> 
-      let ival = read_exp() in
-      let xval = read_exp() in
-      Times(ival, xval)
-    | "divide" -> 
-      let ival = read_exp() in
-      let xval = read_exp() in
-      Divide(ival, xval)
-    | "new" -> (*have to change this*)
-      let ival = read_id() in
-      New(ival)
-    | "identifier" ->
-      let ival = read_id () in
-      Identifier(ival)
-    | x -> (* Fixme: do all of the others*)
-      printf "%s\n" eloc;
-      failwith ("expression kind unhandled: " ^ x)
+    let static_type =
+      match read () with
+      | "SELF_TYPE" -> Some (SELF_TYPE "SELF_TYPE") (* Replace "SELF_TYPE" with context if needed *)
+      | typ -> Some (Class typ)
+    in
+    let ekind =
+      match read () with
+      | "integer" ->
+          let ival = read () in
+          Integer ival
+      | "string" ->
+          let sval = read () in
+          String sval
+      | "bool" ->
+          let bval = read () in
+          Bool bval
+      | "plus" ->
+          let left = read_exp read in
+          let right = read_exp read in
+          Plus (left, right)
+      | "times" ->
+          let left = read_exp read in
+          let right = read_exp read in
+          Times (left, right)
+      | "divide" ->
+          let left = read_exp read in
+          let right = read_exp read in
+          Divide (left, right)
+      | "minus" ->
+          let left = read_exp read in
+          let right = read_exp read in
+          Minus (left, right)
+      | "let" ->
+          let num_bindings = int_of_string (read ()) in
+          let bindings =
+            let rec parse_bindings n acc =
+              if n = 0 then List.rev acc
+              else
+                let binding_type = read () in
+                let binding =
+                  match binding_type with
+                  | "let_binding_no_init" ->
+                      let let_var = (read (), read ()) in
+                      let var_type = (read (), read ()) in
+                      (let_var, var_type, None)
+                  | "let_binding_init" ->
+                      let let_var = (read (), read ()) in
+                      let var_type = (read (), read ()) in
+                      let init_exp = read_exp read in
+                      (let_var, var_type, Some init_exp)
+                  | _ -> failwith ("Invalid let binding type: " ^ binding_type)
+                in
+                parse_bindings (n - 1) (binding :: acc)
+            in
+            parse_bindings num_bindings []
+          in
+          let let_body = read_exp read in
+          Let (bindings, let_body)
+      | "block" ->
+          let num_exps = int_of_string (read ()) in
+          let rec parse_block n acc =
+            if n = 0 then List.rev acc
+            else
+              let expr = read_exp read in
+              parse_block (n - 1) (expr :: acc)
+          in
+          let exprs = parse_block num_exps [] in
+          Block exprs
+      | "case" ->
+          let test_exp = read_exp read in
+          let num_cases = int_of_string (read ()) in
+          let rec parse_cases n acc =
+            if n = 0 then List.rev acc
+            else
+              let var = (read (), read ()) in
+              let var_type = (read (), read ()) in
+              let case_body = read_exp read in
+              parse_cases (n - 1) ((var, var_type, case_body) :: acc)
+          in
+          let cases = parse_cases num_cases [] in
+          Case (test_exp, cases)
+      | "identifier" ->
+          let id = (read (), read ()) in
+          Identifier id
+      | "new" ->
+          let id = (read (), read ()) in
+          New id
+      | "if" ->
+          let if_exp = read_exp read in
+          let then_exp = read_exp read in
+          let else_exp = read_exp read in
+          If (if_exp, then_exp, else_exp)
+      | "while" ->
+          let loop_exp = read_exp read in
+          let pool_exp = read_exp read in
+          While (loop_exp, pool_exp)
+      | "assign" ->
+          let var = (read (), read ()) in
+          let rhs = read_exp read in
+          Assign (var, rhs)
+      | "isvoid" ->
+          let isvoid_exp = read_exp read in
+          Isvoid isvoid_exp
+      | "dynamic_dispatch" ->
+          let expr = read_exp read in
+          let metho = (read (), read ()) in
+          let num_args = int_of_string (read ()) in
+          let rec parse_args n acc =
+            if n = 0 then List.rev acc
+            else
+              let arg = read_exp read in
+              parse_args (n - 1) (arg :: acc)
+          in
+          let args = parse_args num_args [] in
+          Dynamic_Dispatch (expr, metho, args)
+      | "static_dispatch" ->
+          let expr = read_exp read in
+          let ftype = (read (), read ()) in
+          let metho = (read (), read ()) in
+          let num_args = int_of_string (read ()) in
+          let rec parse_args n acc =
+            if n = 0 then List.rev acc
+            else
+              let arg = read_exp read in
+              parse_args (n - 1) (arg :: acc)
+          in
+          let args = parse_args num_args [] in
+          Static_Dispatch (expr, ftype, metho, args)
+      | "self_dispatch" ->
+          let metho = (read (), read ()) in
+          let num_args = int_of_string (read ()) in
+          let rec parse_args n acc =
+            if n = 0 then List.rev acc
+            else
+              let arg = read_exp read in
+              parse_args (n - 1) (arg :: acc)
+          in
+          let args = parse_args num_args [] in
+          Self_Dispatch (metho, args)
+      | "lt" ->
+          let left = read_exp read in
+          let right = read_exp read in
+          LT (left, right)
+      | "le" ->
+          let left = read_exp read in
+          let right = read_exp read in
+          LE (left, right)
+      | "eq" ->
+          let left = read_exp read in
+          let right = read_exp read in
+          EQ (left, right)
+      | "negate" ->
+          let negate_exp = read_exp read in
+          Negate negate_exp
+      | "not" ->
+          let not_exp = read_exp read in
+          Not not_exp
+      | "internal" ->
+          let type_loc = read () in
+          let typ_name = read () in
+          let class_name = read () in
+          let metho_name = read () in
+          Internal ((type_loc, typ_name), class_name, metho_name)
+      | _ -> failwith "Unrecognized expression kind"
     in
     {
       loc = eloc;
@@ -217,7 +254,7 @@ and exp_kind =
       static_type = None;
     }
     in
-    let read_class_map=
+    let read_class_map =
       let class_map : class_map = Hashtbl.create 10 in
       let _ = read () in
       let num_classes = int_of_string (read ()) in
@@ -236,7 +273,7 @@ and exp_kind =
                   | "initializer" ->
                       let aname = read () in
                       let atype = read () in
-                      let init_exp = read_exp () in
+                      let init_exp = read_exp read in
                       (aname, atype, Some init_exp)
                   | "no_initializer" ->
                       let aname = read () in
